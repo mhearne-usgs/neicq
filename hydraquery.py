53ab9a4fe82b970d32d3652fe0a3b8efd52c5fe6
Michael Hearne <mhearne@usgs.gov>
2014-09-10 13:41:24 -0600

    testing how many events were created

--- hydraquery.py
+++ hydraquery.py
@@ -12,127 +12,12 @@ import cx_Oracle
 TIMEFMT = '%Y-%m-%d %H:%M:%S'
 DEBUG = True
 
-def getQueries(homedir):
-    #assume the template file is a bunch of SQL queries bracketed by C++ style
-    #comment blocks.  Whenever a comment block starts, if we've been processing
-    #lines as a query, that query stops and gets added to the list.
-    queryfile = os.path.join(homedir,'query.template')
-    f = open(queryfile,'rt')
-    queries = []
-    isquery = False
-    iscomment = False
-    thisquery = ''
-    for line in f.readlines():
-        if line.strip().startswith('/*'):
-            if not line.strip().endswith('*/'): #we're starting a comment block
-                iscomment = True
-            if isquery: #then we just finished a query
-                if len(thisquery.strip()):
-                    queries.append(thisquery)
-                thisquery = ''
-                isquery = False
-                continue
-            else:
-                continue
-        if line.strip().endswith('*/') and iscomment: #we're ending a comment block
-            iscomment = False
-            continue
-        if not iscomment:
-            isquery = True
-            thisquery += line
-    if isquery:
-        queries.append(thisquery) #grab the last one, if not followed by comment block     
-    f.close()
-    return queries
-
-def getMostRecentPDE(cursor):
-    query = 'select get_pde_from_ot(max(torigin)) from all_events_info_no_nph where iworkflowstatus = 8192'
+def getEventCount(cursor):
+    query = 'select count(idEvent) from PDEHydra'
     cursor.execute(query)
-    pdestr = cursor.fetchone()[0]
-    return pdestr
+    nevents = cursor.fetchone()[0]
+    return nevents
 
-def getPDERange(cursor,pdenumber):
-    if DEBUG:
-        pdemin = 1397600000
-        pdemax = 1397610000
-    else:
-        query = 'select get_start_of_pde(pdenumber), get_end_of_pde(pdenumber) from dual'
-        cursor.execute(query)
-        row = cursor.fetchone()
-        pdemin = row[0]
-        pdemax = row[1]
-    return (pdemin,pdemax)
-
-def emptyTable(cursor,db):
-    cursor.execute('TRUNCATE table HDB_MONITOR.PDEHydra')
-    db.commit()
-    cursor.execute('SELECT count(idEvent) FROM HDB_MONITOR.PDEHydra')
-    nrows = cursor.fetchone()[0]
-    if nrows != 0:
-        return False
-    return True
-
-def insertBaseRecords(cursor,db,tstart,tend):
-    query = '''insert into HDB_MONITOR.PDEHydra(idEvent, OT, Lat, Lon, Depth, Mag, idBHPF, iPDENum)
-    (select bhp.idEvent, tOrigin, dLat, dLon, dDepth, dPrefMag, bhp.idBHPellet, get_pde_from_ot(tOrigin)
-    from all_bhpelletdescs bhp, 
-	(
-	select idevent, max(idbhpellet) idbhpellet from all_bhpelletdescs 
-	where iworkflowstatus >= 8192*65536
-	group by idevent
-	) mbhp
-    where bhp.idBHPellet = mbhp.idBHPellet
-    and bhp.tOrigin between %i and %i
-    )''' % (tstart,tend)
-    query = query.replace('\n',' ')
-    query = query.replace('\t',' ')
-    cursor.execute(query)
-    db.commit()
-    cursor.execute('SELECT count(idEvent) FROM HDB_MONITOR.PDEHydra')
-    nrows = cursor.fetchone()[0]
-    return nrows
-
-def addInitialDetection(cursor,db):
-    query = '''update HDB_MONITOR.PDEHydra p
-    set IDAOINITIAL = 
-    (select min(ao2.idactualorigin) 
-    from  all_actualorigin_info AO1, all_actualorigin_info AO2
-    where AO1.idActualOrigin = (select min(idactualorigin) idAO 
-    from all_event_origin_info aeoi
-    where aeoi.idevent = p.idevent
-    and torigininserted = (select min(torigininserted) from all_event_origin_info where idevent = p.idevent and bitand(iAuthorType, 256) = 0)
-    and bitand(iAuthorType, 256) = 0
-    )
-    and AO1.sauthoreventid = AO2.sauthoreventid
-	and AO1.idAuthor = AO2.idAuthor
-    )
-    where idEvent IS NOT NULL'''
-    query = query.replace('\r',' ')
-    query = query.replace('\t',' ')
-    cursor.execute(query)
-    db.commit()
-    
-
-def getGlassDetection(cursor,db):
-    query = '''update HDB_MONITOR.PDEHydra p
-    set IDAOGLASS = 
-    (select min(ao2.idactualorigin) 
-    from All_actualorigin_info AO1, All_actualorigin_info AO2
-    where AO1.idActualOrigin = (select min(idactualorigin) idAO 
-    from all_event_origin_info aeoi
-    where aeoi.idevent = p.idevent
-    and torigininserted = (select min(torigininserted) from all_event_origin_info where idevent = p.idevent and sauthornamehr like '%GLASS%' and sinstcode='NEIC')
-    and sauthornamehr like '%GLASS%' and sinstcode='NEIC'
-    )
-    and AO1.sauthoreventid = AO2.sauthoreventid
-	and AO1.idAuthor = AO2.idAuthor
-    )
-    where idEvent IS NOT NULL'''
-    query = query.replace('\r',' ')
-    query = query.replace('\t',' ')
-    cursor.execute(query)
-    db.commit()
-    
 def main():
     homedir = os.path.dirname(os.path.abspath(__file__)) #where is this script?
     lastfile = os.path.join(homedir,'lastprocessed.txt')
@@ -168,7 +53,9 @@ def main():
         error, = exc.args
         print "Oracle-Error-Code:", error.code
         print "Oracle-Error-Message:", error.message
-    
+
+    nevents = getEventCount(cursor)
+    print '%i events created in pde report table.' % nevents
     cursor.close()
     db.close()
 
