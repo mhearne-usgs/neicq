0c351f2b8d31d43da1249cdadec1a5617d83c33b
Michael Hearne <mhearne@usgs.gov>
2014-09-11 09:04:44 -0600

    bug fix

--- hydraquery.py
+++ hydraquery.py
@@ -248,8 +248,8 @@ def main():
     qkeys.sort()
     for key in qkeys:
         qstart,qend = QUARTERS[key]
-        startpde = str(datetime.now().year) + '%02i' % qstart
-        endpde = str(datetime.now().year) + '%02i' % qend
+        startpde = int(str(datetime.now().year) + '%02i' % qstart)
+        endpde = int(str(datetime.now().year) + '%02i' % qend)
         qendstr = str(datetime.now().year)+key
         if qendstr <= lastquarter:
             break 
