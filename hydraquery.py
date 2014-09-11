efe65d7248efe9a955e25aa4ac621f66af0d2ec9
Michael Hearne <mhearne@usgs.gov>
2014-09-11 09:01:58 -0600

    comparison of quarters

--- hydraquery.py
+++ hydraquery.py
@@ -248,6 +248,8 @@ def main():
     qkeys.sort()
     for key in qkeys:
         qstart,qend = QUARTERS[key]
+        startpde = str(datetime.now().year) + '%0i' % qstart
+        endpde = str(datetime.now().year) + '%0i' % qend
         qendstr = str(datetime.now().year)+key
         if qendstr <= lastquarter:
             break 
